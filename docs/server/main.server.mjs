17de005e5bb3881dc6e096f8b42225c17d176b1b
jennykan228 <jennykan228@gmail.com>
2025-11-04 18:01:37 +0800

    Add additional jewelry store images and update project details.

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GSR2DN2F.mjs";import"./chunk-OIRAGZRX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-XQ5B76QJ.mjs";import"./chunk-OIRAGZRX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
