f3aca065531221b363dd2c977e8a2da91e49a23c
jennykan228 <jennykan228@gmail.com>
2025-11-19 17:24:26 +0800

    add wordpress project

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XQ5B76QJ.mjs";import"./chunk-OIRAGZRX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-FZXWAZVQ.mjs";import"./chunk-OIRAGZRX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
