4b7fe8fd7abd9450ef62296c31945297327fae3d
jennykan228 <jennykan228@gmail.com>
2026-01-04 19:31:36 +0800

    add django project

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FZXWAZVQ.mjs";import"./chunk-OIRAGZRX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-O6YD4F4X.mjs";import"./chunk-OIRAGZRX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
